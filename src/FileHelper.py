f5d705eeb9e1200a05780b45f4319d7cfdf09ff2
Alex Chung <sky9en@virginia.edu>
2024-06-04 13:49:40 -0400

    figuring out excel exporting

--- src/FileHelper.py
+++ src/FileHelper.py
@@ -1,36 +1,57 @@
-from tkinter import filedialog
+from tkinter import messagebox, filedialog
 import pandas as pd
-from bs4 import BeautifulSoup
-import ListApplication
+import os
+import csv
 
+INSURANCELIST = None
+ACTIVEPATIENTLIST = None
+isInsurance = False
+isPatients = False
 
 @staticmethod
-def import_file(labelToSet):
-    file_path = filedialog.askopenfilename(title="Select a file", filetypes=[("Excel files", "*.xlsx")])
+def import_file(labelToSet, listname):
+    global INSURANCELIST, ACTIVEPATIENTLIST, isInsurance, isPatients
+    file_path = filedialog.askopenfilename(title="Select a file", filetypes=[("Excel Files", "*.xlsx")])
     if file_path:
-        clonedpath = file_path
         labelToSet.config(text = file_path)
-        if (labelToSet is ListApplication.insurance_text):
-            print("hi")
-            reading_spreadsheet(file_path)
-        elif (labelToSet is ListApplication.patient_text):
-            reading_spreadsheet(file_path)
+        if (listname == "insurance"):
+            INSURANCELIST = reading_spreadsheet(file_path)
+            isInsurance = True
+        elif (listname == "activepatient"):
+            ACTIVEPATIENTLIST = reading_spreadsheet(file_path)
+            isPatients = True
 
+@staticmethod
 def get_file_name_from_path(filepath):
     returnstring = filepath
-    while (not returnstring.find("/") == -1 ):
-        returnstring = returnstring[returnstring.find("/")+1, len(returnstring)]
-    print(returnstring)
+    while (not returnstring.find("/") == -1):
+        returnstring = returnstring[returnstring.find("/")+1:len(returnstring)]
     return returnstring    
     
 
+@staticmethod
 def reading_spreadsheet(spreadsheetpath):
-    paths = []
-    mySheet = 'Sheet Name'
-    filename = spreadsheetpath
-    data = BeautifulSoup()
+    data = pd.read_excel(spreadsheetpath, index_col = 0)
+    return data
 
 
 @staticmethod
-def generate_list(list1, list2):
-    return list2-list1
+def generate_lists():
+    global ACTIVEPATIENTLIST, INSURANCELIST, isInsurance, isPatients
+    print(INSURANCELIST.head(n=5))
+    if isInsurance == False or isPatients == False:
+        messagebox.showerror("Files Not Provided", "Please provide a .xlsx file for both lists ")
+        return False
+    
+    fln = filedialog.asksaveasfilename(initialdir=os.getcwd(), title="Save Excel File", filetypes=(("Excel File", ".xlsx"), ("All Files", ".*")))
+    fle = ".xlsx"
+    fln = fln if fln[-len(fle):].lower() == fle else fln + fle
+    print(fln)
+    with pd.ExcelWriter(fln) as writer:
+        INSURANCELIST.to_excel(writer, sheet_name='List')
+
+
+    # file_name = 'data.xlsx'
+
+    # with pd.ExcelWriter(file_name) as writer:
+    #     INSURANCELIST.to_excel(writer, sheet_name='List', index = False)
--- src/ListApplication.py
+++ src/ListApplication.py
@@ -1,44 +1,47 @@
 import tkinter as tk
+from tkinter import filedialog
 from FileHelper import *
 from functools import partial
 
-HEIGHT = 300
-WIDTH = 500
+class MyWindow:
+    def __init__(self, parent):
+        self.parent = parent
 
-INSURANCELIST = None
-ACTIVEPATIENTLIST = None
+        self.HEIGHT = 300
+        self.WIDTH = 500
 
-root = tk.Tk()
+        canvas = tk.Canvas(root, height=self.HEIGHT, width=self.WIDTH, bg="#ADD8E6")
+        canvas.pack(fill="both", expand = True)
 
-canvas = tk.Canvas(root, height=HEIGHT, width=WIDTH, bg="#ADD8E6")
-canvas.pack(fill="both", expand = True)
+        upframe = tk.Frame(root, bg="#FFD580")
+        upframe.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.5)
 
-upframe = tk.Frame(root, bg="#FFD580")
-upframe.place(relx=0.05, rely=0.05, relwidth=0.9, relheight=0.5)
+        downframe = tk.Frame(root, bg="#FFD580")
+        downframe.place(relx= 0.05, rely=0.6, relwidth=0.9, relheight = 0.35)
 
-downframe = tk.Frame(root, bg="#FFD580")
-downframe.place(relx= 0.05, rely=0.6, relwidth=0.9, relheight = 0.35)
+        buttonframe = tk.Frame(upframe, bg="#FFD580")
+        buttonframe.pack(side="left")
 
-buttonframe = tk.Frame(upframe, bg="#FFD580")
-buttonframe.pack(side="left")
+        insuranceframe = tk.Frame(buttonframe)
+        insuranceframe.pack(fill = "x")
 
-insuranceframe = tk.Frame(buttonframe)
-insuranceframe.pack(fill = "x")
+        patientframe = tk.Frame(buttonframe)
+        patientframe.pack(fill = "x")
 
-patientframe = tk.Frame(buttonframe)
-patientframe.pack(fill = "x")
+        insurance_text = tk.Label(insuranceframe)
+        insurance_button = tk.Button(insuranceframe, text="Select Insurance List File", command=partial(import_file, insurance_text, "insurance"))
+        insurance_button.pack(side = "left")
+        insurance_text.pack(side = "left")
 
-global insurance_text, patient_text
-insurance_text = tk.Label(insuranceframe)
-insurance_button = tk.Button(insuranceframe, text="Select Insurance List File", command=partial(import_file, insurance_text))
-insurance_button.pack(side = "left")
-insurance_text.pack(side = "left")
+        patient_text = tk.Label(patientframe)
+        patient_button = tk.Button(patientframe, text="Select Patient List File", command=partial(import_file, patient_text, "activepatient"))
+        patient_button.pack(side = "left")
+        patient_text.pack(side = "left")
 
-patient_text = tk.Label(patientframe)
-patient_button = tk.Button(patientframe, text="Select Patient List File", command=partial(import_file, patient_text))
-patient_button.pack(side = "left")
-patient_text.pack(side = "left")
+        generate_button = tk.Button(downframe, text="Generate Lists", command=generate_lists)
+        generate_button.pack(side = "top")
 
-#generate_button = tk.Button(downframe, text="Generate Lists", command=partial())
-
-root.mainloop()
+if __name__ == '__main__':
+    root = tk.Tk()
+    top =  MyWindow(root)
+    root.mainloop()
